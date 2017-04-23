111a0597a26ae9b30cafc51f5547d32784dce7ed
Iegor Maiorov <maiorov.prog@gmail.com>
2017-04-23 22:58:10 +0300

    added courses components

--- app/courses.component.ts
+++ app/courses.component.ts
@@ -4,8 +4,18 @@ import {Component} from "angular2/core";
 //decoratotor( aka annotation in java)
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'
+    template: `
+        <h2>Courses</h2>
+        {{title}}
+        <ul>
+            <li *ngFor="#course of courses">
+                {{course}}
+            </li>
+        </ul>
+    `
     })
 export class CoursesComponent {
+    title: string = "The title of courses page";
+    courses = ["Course1", "Course2", "Course3"];
 
 }
