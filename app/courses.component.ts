8854b25737eabf261f0113f543749c98260ff1d9
Iegor Maiorov <maiorov.prog@gmail.com>
2017-04-23 22:51:18 +0300

    added component

---
+++ app/courses.component.ts
@@ -0,0 +1,11 @@
+
+import {Component} from "angular2/core";
+
+//decoratotor( aka annotation in java)
+@Component({
+    selector: 'courses',
+    template: '<h2>Courses</h2>'
+    })
+export class CoursesComponent {
+
+}
